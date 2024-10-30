8bdadd71133e144eb312413499f804cbf095e463
DAVID\david <davidalejangp@gmail.com>
2024-10-30 17:46:39 -0500

    SOLUCION: al git anterior y en vercel modificar la carpeta a 'public' y agregar npm run build

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,7 +14,6 @@ class ProductController extends Controller
     public function index()
     {
         $products = Product::all();
-        dd($products);
         return view('products.index', compact('products'));
     }
 
