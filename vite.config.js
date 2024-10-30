573b4423dcd98ca5c0da5de0d82952ee92c4212d
DAVID\david <davidalejangp@gmail.com>
2024-10-30 16:15:15 -0500

    Agrega archivos css y js a vite.manifest

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,22 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
+    build: {
+        manifest: true,
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                "resources/css/app.css",
+                "resources/css/app-dark.css",
+                "resources/css/iconly.css",
+                "resources/js/helper/isDesktop.js",
+                "resources/js/app.js",
+                "resources/js/bootstrap.js",
+                "resources/js/dark.js",
+                "resources/js/sidebar.js",
+            ],
             refresh: true,
         }),
     ],
